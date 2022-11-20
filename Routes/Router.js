2235ac5fbff391a64e8759fa0be78b51fc5bfdbe
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2022-11-20 22:32:46 +0000

    Commit #6 - Router Stuff and Navbar Filler

--- Routes/Router.js
+++ Routes/Router.js
@@ -1,5 +1,9 @@
-const indexController = require("../Views/Controllers/indexController");
+const indexController = require('../Views/Controllers/indexController');
+const esportsGame = require('../Views/Controllers/esportsGames');
+const esportsPlayers = require('../Views/Controllers/esportsPlayers');
 
 module.exports = (app) => {
-    app.use("/", indexController);
+    app.get('/', indexController);
+    app.get('/esports-games/', esportsGame);
+    app.get('/esports-players/', esportsPlayers);
 };
---
+++ Utils/NavbarUtilFiller.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+    return {
+        page: {
+            homePageTitle: 'Home Page',
+            navLinkTitleOne: 'Esports Games',
+            navLinkTitleTwo: 'Esports Players',
+            navLinkOneHref: '/esports-games',
+            navLinkTwoHref: '/esports-players',
+        },
+    };
+};
