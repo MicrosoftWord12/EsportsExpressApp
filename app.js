bec0d9f47bc6c67b1a31c1d17f36eb0a119e761b
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2022-11-21 22:29:46 +0000

    Commit #10 - App.js rewrite?

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const { PORT } = require("./config");
-const ServerMessage = require("./Utils/ServerMessage");
-const bodyParser = require("body-parser");
-const Router = require("./Routes/Router");
+const express = require('express');
+const { PORT } = require('./config');
+const ServerMessage = require('./Utils/ServerMessage');
+const bodyParser = require('body-parser');
+const FileController = require('./Utils/FileController');
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-Router(app);
+// Router(app);
+FileController(app);
 
 app.listen(PORT, () => {
     console.log(`${ServerMessage(`Server is running on port ${PORT}`)}`);
